ac42eb3d07192e7f3d156b24a59ab40ec1580778
Gucci Gangnam <diviner02hive@icloud.com>
2024-06-07 13:11:49 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Hello from READ ME
---
+++ React-Front/src/App.jsx
@@ -0,0 +1,20 @@
+// IMPORTS //
+// React 
+import { useState } from 'react'
+// RRD
+
+// Styles
+import './App.css'
+
+// COMPONENT //
+
+function App() {
+
+  return (
+    <div className='App'>
+
+    </div>
+  )
+}
+
+export default App
